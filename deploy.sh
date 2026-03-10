30fb677bb56d2a0b1824153484caf7f7a950a6ae
Augustin Gjini <augustin.gjini@pm.me>
2026-03-10 12:14:33 +0100

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
+set -e
 
 rm -Rf DbModelGenerator/bin
 rm -Rf DbModelGenerator/obj
 
-dotnet build DbModelGenerator/DbModelGenerator.csproj
-
-dotnet build -c Release
+dotnet pack DbModelGenerator/DbModelGenerator.csproj -c Release
 
 # Be sure to add nuget API key before in your bashrc
 # vi ~/.bashrc
 # export NUGET_AUTH_KEY=XXXXX
 
-dotnet nuget push DbModelGenerator/bin/Release/*.nupkg -k ${NUGET_AUTH_KEY} -s https://api.nuget.org/v3/index.json
-
+dotnet nuget push DbModelGenerator/bin/Release/*.nupkg -k ${NUGET_AUTH_KEY} -s https://api.nuget.org/v3/index.json --skip-duplicate
